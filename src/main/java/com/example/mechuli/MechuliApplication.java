00226290e410e83a8a9ccf7eb648e7e2db2b3800
vsuminv <sumin@naver.com>
2024-09-02 11:46:28 +0900

    first commit

---
+++ src/main/java/com/example/mechuli/MechuliApplication.java
@@ -0,0 +1,13 @@
+package com.example.mechuli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MechuliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MechuliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mechuli/MechuliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mechuli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MechuliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
