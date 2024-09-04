cc002228ac5d223c0b400724c5320e7ec88c2b06
ruukr8080@gmail.com <ruukr8080@gmail.com>
2024-09-04 19:06:08 +0900

    [feat] : set security

--- src/main/java/com/example/mechuli/controller/TestController.java
+++ src/main/java/com/example/mechuli/controller/TestController.java
@@ -1,27 +1,24 @@
-package com.example.mechuli.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class TestController {
-    @GetMapping("/test")
-    public String ttt(Model model) {
-
-
-        model.addAttribute("data","1");
-        return "home";
-    }
-    @GetMapping("/join")
-    public String jj(Model model) {
-
-
-        model.addAttribute("data","1");
-        return "joinForm";
-    }
-
-
-
-
-}
+//package com.example.mechuli.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//public class TestController {
+//    @GetMapping("/test")
+//    public String ttt(Model model) {
+//
+//
+//        model.addAttribute("data","1");
+//        return "home";
+//    }
+//    @GetMapping("/join")
+//    public String jj(Model model) {
+//
+//
+//        model.addAttribute("data","1");
+//        return "joinForm";
+//    }
+//
+//}
