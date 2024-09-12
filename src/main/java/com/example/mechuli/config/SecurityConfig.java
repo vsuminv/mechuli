09e902efa3bf9e100659da40ce3128a8c57ddc7e
ruukr8080@gmail.com <ruukr8080@gmail.com>
2024-09-12 18:35:23 +0900

    secret

--- src/main/java/com/example/mechuli/config/SecurityConfig.java
+++ src/main/java/com/example/mechuli/config/SecurityConfig.java
@@ -3,11 +3,14 @@ package com.example.mechuli.config;
 
 import com.example.mechuli.service.UserService;
 import lombok.RequiredArgsConstructor;
+//import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.boot.autoconfigure.security.reactive.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -31,7 +34,7 @@ public class SecurityConfig {
 //                .headers(AbstractHttpConfigurer::disable)
 
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/js/**","/auth/**", "/css/**", "/image/**","/tailwind.css","/img/**.png", "/thymeleaf/**","/csrf-token", "/ajaxCheckId", "/ajaxCheckNickname").permitAll()
+                        .requestMatchers("/js/**","/auth/**", "/css/**", "/img/**","/image/**","/tailwind.css", "/thymeleaf/**","/csrf-token", "/ajaxCheckId", "/ajaxCheckNickname").permitAll()
                         .requestMatchers("/login","/join","/wellcomePage").permitAll()
 //                        .anyRequest().permitAll()
                         .anyRequest().authenticated()
