04bd200cd389490a2f682528bf3b9ecb0de8892c
Choi TaeHyung <taehyoung0528@gmail.com>
2024-09-13 15:04:59 +0900

    코ëãìì½리뷰소스 ë°백업

--- src/main/java/com/example/mechuli/dto/ReviewDTO.java
+++ src/main/java/com/example/mechuli/dto/ReviewDTO.java
@@ -1,4 +1,29 @@
 package com.example.mechuli.dto;
 
+import com.example.mechuli.domain.Restaurant;
+import com.example.mechuli.domain.Review;
+import com.example.mechuli.domain.UserDAO;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ReviewDTO {
+    private Long reviewId;
+    private String content;
+    private Long restaurantId;  // 이 값은 JSON에서 Long 타입으로 파싱됨
+    private Long userIndex;     // 이 값 역시 Long 타입으로 파싱됨
+    private List<String> reviewImg;
+
+    public ReviewDTO(Review review){
+        this.reviewId = review.getReviewId();
+        this.content = review.getContent();
+        this.restaurantId = review.getRestaurant().getRestaurantId();
+        this.userIndex = review.getUserIndex().getUserIndex();
+        this.reviewImg = review.getReviewImg();
+    }
 }
