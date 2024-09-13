56634ead3276c64a8de4ada3a09e77afc8f263e5
Choi TaeHyung <taehyoung0528@gmail.com>
2024-09-13 17:31:26 +0900

    리뷰 이미지 업로드 로직 추가중

--- src/main/java/com/example/mechuli/repository/ReviewRepository.java
+++ src/main/java/com/example/mechuli/repository/ReviewRepository.java
@@ -1,8 +1,12 @@
 package com.example.mechuli.repository;
 
+import com.example.mechuli.domain.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface ReviewRepository extends JpaRepository {
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+//    boolean chekcAuthed(String authUser);
 }
