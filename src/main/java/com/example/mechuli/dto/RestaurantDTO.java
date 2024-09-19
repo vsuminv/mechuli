7d161b61167edb0c9ba9f7bec931e4bc63bef639
Justin-S-Hong <madwolves98@gmail.com>
2024-09-19 19:33:01 +0900

    [Refactor] RestaurantDTO 수정

--- src/main/java/com/example/mechuli/dto/RestaurantDTO.java
+++ src/main/java/com/example/mechuli/dto/RestaurantDTO.java
@@ -18,6 +18,7 @@ public class RestaurantDTO {
     private String open_time;
     private String close_time;
     private String address;
+    private Long category_id;
     private String category_name;
 
     public RestaurantDTO(Restaurant restaurant){
@@ -25,11 +26,19 @@ public class RestaurantDTO {
         this.name = restaurant.getName();
         this.img_url = restaurant.getImageUrl();
 
+
         // null값 들어있는 카테고리가 있어서 조건문으로 처리
-        if (restaurant.getRestaurantCategory() != null) {
+        if (restaurant.getRestaurantCategory()!= null) {
+            this.category_id = restaurant.getRestaurantCategory().getCategoryId();
             this.category_name = restaurant.getRestaurantCategory().getCategoryName();
         } else {
+            this.category_id = null; // 또는 적절한 기본값으로 설정
             this.category_name = "없음";
         }
+//        if (restaurant.getRestaurantCategory() != null) {
+//            this.category_name = restaurant.getRestaurantCategory().getCategoryName();
+//        } else {
+//            this.category_name = "없음";
+//        }
     }
 }
