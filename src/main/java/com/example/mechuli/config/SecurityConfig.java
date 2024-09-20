16a3a4d837ad8cbbea982149b278499bc9b5d9f7
vsuminv <sumin@naver.com>
2024-09-20 14:16:26 +0900

    Refact: 메인페이지 디자인 수정

--- src/main/java/com/example/mechuli/config/SecurityConfig.java
+++ src/main/java/com/example/mechuli/config/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
 //                                .requestMatchers("/joinPage/**","/","/myPage/**").permitAll()
 //
 
-                                .requestMatchers("/js/**","/join", "/home", "/auth/**", "/api/**", "/css/**", "/img/**", "/image/**", "/tailwinds.css", "/thymeleaf/**", "/csrf-token", "/ajaxCheckId", "/ajaxCheckNickname", "/ajax/**", "/boardPage").permitAll()
+                                .requestMatchers("/js/**","/join", "/home", "/auth/**", "/api/**", "/css/**", "/img/**", "/image/**", "/tailwinds.css", "/thymeleaf/**", "/csrf-token", "/ajaxCheckId", "/ajaxCheckNickname", "/ajax/**").permitAll()
 //                                .requestMatchers("/js/**","/home","/auth/**","/api/**", "/css/**", "/img/**","/image/**","/tailwinds.css", "/thymeleaf/**","/csrf-token", "/ajaxCheckId", "/ajaxCheckNickname").permitAll()
                                 .requestMatchers("/joinPage","/").permitAll()
 
